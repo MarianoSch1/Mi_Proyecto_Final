087b50d790367a5367e6fedb2aea8494bb4766ab
MarianoSch1 <schvartzman.91@hotmail.com>
2022-10-18 02:46:06 -0300

    Cuarto commit agregados nuevos familiares

--- seed_data.py
+++ seed_data.py
@@ -1,8 +1,8 @@
 from ejemplo.models import Familiar
 
-Familiar(nombre="Rosario", direccion="Rio Parana 745", numero_pasaporte=123123).save()
-Familiar(nombre="Alberto", direccion="Rio Parana 745", numero_pasaporte=890890).save()
-Familiar(nombre="Samuel", direccion="Rio Parana 745", numero_pasaporte=345345).save()
-Familiar(nombre="Florencia", direccion="Rio Parana 745", numero_pasaporte=567567).save()
+Familiar(nombre="Ana", direccion="Juramento 3045", numero_pasaporte=123123).save()
+Familiar(nombre="Victor", direccion=" Campana 2001", numero_pasaporte=890890).save()
+Familiar(nombre="Saul", direccion="Angel Gallardo 75", numero_pasaporte=345345).save()
+Familiar(nombre="Felipe", direccion="Campana 3045", numero_pasaporte=567567).save()
 
 print("Se cargo con éxito los usuarios de pruebas")
