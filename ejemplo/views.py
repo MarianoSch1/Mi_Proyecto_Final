ab0435054edbd11e379f6dd062a1df723c7833f1
MarianoSch1 <schvartzman.91@hotmail.com>
2022-10-18 02:09:57 -0300

    segundo commit del proyecto revisar index_dos

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from ejemplo.models import Familiar
 
@@ -7,3 +8,8 @@ def monstrar_familiares(request):
     lista_familiares = Familiar.objects.all()
     return render(request, "ejemplo/familiares.html", {"lista_familiares": lista_familiares})
 # Create your views here.
+def index_dos(request, nombre, apellido):
+    return render(request, "ejemplo/saludar.html", {"nombre":nombre}, {"apellido":apellido})
+
+def index_tres(request):
+    return render(request, "ejemplo/saludar.html", {"notas":[1,2,3,4,5,6,7,8]})
